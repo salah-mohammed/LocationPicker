7c3a251e0c5e011ca083ddd599f33b271f89675e
Salah <Salahalimohamed1995@gmail.com>
2020-01-21 15:45:18 +0200

    Initial Commit

---
+++ Salah_Mohammed/Projects/IOSProjects/Own/LocationPickeer/LocationPickeerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationPickeerExample
+//
+//  Created by Salah on 1/21/20.
+//  Copyright © 2020 Salah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
